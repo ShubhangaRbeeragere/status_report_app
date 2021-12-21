7f787947ca1627addbfbae885eeeebe127e58a36
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2021-12-21 13:31:54 +0530

    Added toDoList.services.ts file with all the function templates. >> added body-parser

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import bodyParser from "body-parser";
+
 import {makeConnection} from "../src/init/createConnection";
 dotenv.config();
 if (!process.env.PORT) {
@@ -14,6 +16,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 makeConnection
 .then(
---
+++ node/src/services/toDoList.services.ts
@@ -0,0 +1,26 @@
+import { getManager } from "typeorm";
+import ToDoContent from "../model/entity/toDoContent"
+import ToDoList from "../model/entity/toDoList"
+import {Request, Response} from "express";
+
+//get all the to do list rows
+export const getAll = function(req: Request, res: Response){
+
+}
+//get all the to do list rows
+export const getOnly = function(req: Request, res: Response){
+
+}
+//get one row from to do list
+export const addData = function(req: Request, res: Response){
+
+}
+//remove to do list rows
+export const removeData = function(req: Request, res: Response){
+
+}
+
+//update to do list
+export const updateData = function(req: Request, res: Response){
+
+}
