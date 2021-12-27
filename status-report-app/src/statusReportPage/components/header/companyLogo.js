0ec7b281d4c280ad29b8a500a85e9b4e71edf945
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2021-12-27 16:39:28 +0530

    added image to the header and formatted the companyLogo

--- status-report-app/src/statusReportPage/components/header/companyLogo.js
+++ status-report-app/src/statusReportPage/components/header/companyLogo.js
@@ -1,8 +1,8 @@
 export const CompanyLogo = (props) => {
    return(
        <div className="companyLogo">
-           <div className="name">Name</div>
-           <div className="logo">Logo</div>
+           <div className="name">Status Report</div>
+           <div className="logo"></div>
        </div>
    ) 
 }
