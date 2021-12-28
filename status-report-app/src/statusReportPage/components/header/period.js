53f71fde5455db325f421ee50ce32fb550f95619
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2021-12-28 10:36:29 +0530

    Added calender to home using react-date-picker >> react-date-picker is a library in react for adding and selecting dates

--- status-report-app/src/statusReportPage/components/header/period.js
+++ status-report-app/src/statusReportPage/components/header/period.js
@@ -1,9 +1,20 @@
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react"
 export const Period = (props) => {
+    const [startDate, setStartDate] = useState(new Date()); 
     return(
         <div className="period">
-            <div className="year">Year</div>
+            {/* <div className="year">Year</div>
             <div className="month">Month</div>
-            <div className="day">Day</div>
+            <select name="day" id="day"onClick={(e) => {console.log(e.target.value)}}>
+                <option value="sunday">Sunday</option>
+                <option value="monday">Monday</option>
+                <option value="tuesday">Tuesday</option>
+                <option value="wednesday">Wednesday</option>
+                <option value="thursday">Thursday</option>
+            </select> */}
+            <DatePicker selected={startDate} className="datePicker" onChange={(date) => setStartDate(date)}/>
         </div>
     )
 }
