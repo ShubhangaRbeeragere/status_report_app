3c29ee4d06232f97810b84d55f910d0f3b58a34c
Akhil <akhil.e@travanacoreanalytics.com>
2021-12-28 17:22:02 +0530

    added post,put,getbyDATE,etc methods

---
+++ src/init/connection.ts
@@ -0,0 +1,29 @@
+import { createConnection } from "typeorm";
+import "reflect-metadata";
+import { Achievements } from '../model/entity/Achievements';
+import { Milestones } from '../model/entity/Milestone';
+import { SubAchievements } from '../model/entity/SubAchievements';
+
+
+const sqlConnection = createConnection(
+  {
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "2120Ae@$#",
+    database: "test",
+
+    entities: [
+      Achievements,
+      Milestones,
+      SubAchievements
+
+    ],
+    synchronize: true,
+    logging:true
+  }
+)
+
+
+export default sqlConnection;
---
+++ src/model/entity/Achievements.ts
@@ -0,0 +1,30 @@
+import { Milestones } from './Milestone';
+import {PrimaryGeneratedColumn,
+    Entity,Column,OneToMany} from "typeorm";
+import { SubAchievements } from './SubAchievements';
+
+  @Entity()
+  export class Achievements {
+    
+      @PrimaryGeneratedColumn()
+      achievement_id! : number;
+
+      @Column({ type: "varchar", nullable: false ,unique:true})
+      achievement_name!: string;    
+    
+      @Column({ type: "varchar", nullable: false ,unique:true})
+      content!: string;    
+    
+      @Column({ type: "date", nullable: false })
+      date!: string;
+
+      @OneToMany(type => Milestones, milestones => milestones.milestones_id_fk)
+      milestones_key!: Milestones 
+
+      @OneToMany(type => SubAchievements, subAchievements => subAchievements.sub_achievements_id_fk)
+      sub_achievements_key!: SubAchievements 
+  }
+  
+   
+
+  
