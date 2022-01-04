98aada040f82b6c58ff5c791998ce9a029bd20ec
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2022-01-04 18:30:44 +0530

    changed position fixed to absolute >> changed position of addList button from fixed to absolute >> need to work on fixing the heading and the 'add' button inside .item at the top even on scroll

--- status-report-app/src/statusReportPage/components/toDoList/toDoList.js
+++ status-report-app/src/statusReportPage/components/toDoList/toDoList.js
@@ -1,45 +1,45 @@
 import { Content } from "./content";
 import { AddListButton } from "./addListButton";
 export const ToDoList = (props) => {
-  let toDoList = props.toDoList;
-  let deleteProject = props.deleteProject;
-  let toDoItems =
-    toDoList &&
-    toDoList.map((list) => (
-      <div className="item" key={list.list_id.toString()}>
-        <button
-          title="delete project"
-          className="removeProjectButton"
-          onClick={() => {
-            deleteProject(list.title);
-          }}
-        >
-          <i className="fas fa-times"></i>
-        </button>
-        <h3>{list.title}</h3>
-        <h4>{list.date}</h4>
-        <Content
-          project={list.title}
-          deleteContent={props.deleteContent}
-          contentArray={list.contentKey}
-        />
-        <button
-          title="Add Content"
-          className="addContentButton"
-          onClick={() => {
-            props.toggleContentlist(list.title);
-          }}
-        >
-          <i className="fas fa-plus"></i>
-        </button>
-      </div>
-    ));
+    let toDoList = props.toDoList;
+    let deleteProject = props.deleteProject;
+    let toDoItems =
+        toDoList &&
+        toDoList.map((list) => (
+            <div className="item" key={list.list_id.toString()}>
+                <button
+                    title="delete project"
+                    className="removeProjectButton"
+                    onClick={() => {
+                        deleteProject(list.title);
+                    }}
+                >
+                    <i className="fas fa-times"></i>
+                </button>
+                <h3>{list.title}</h3>
+                <h4>{list.date}</h4>
+                <Content
+                    project={list.title}
+                    deleteContent={props.deleteContent}
+                    contentArray={list.contentKey}
+                />
+                <button
+                    title="Add Content"
+                    className="addContentButton"
+                    onClick={() => {
+                        props.toggleContentlist(list.title);
+                    }}
+                >
+                    <i className="fas fa-plus"></i>
+                </button>
+            </div>
+        ));
 
-  return (
-    <div className="toDoList boxes">
-      <h2>Issues and Risk</h2>
-      {toDoList && toDoItems}
-      {toDoList && <AddListButton toggleAddList={props.toggleAddList} />}
-    </div>
-  );
+    return (
+        <div className="toDoList boxes">
+            <h2>Issues and Risk</h2>
+            <div className="itemWrapper">{toDoList && toDoItems}</div>
+            {toDoList && <AddListButton toggleAddList={props.toggleAddList} />}
+        </div>
+    );
 };
