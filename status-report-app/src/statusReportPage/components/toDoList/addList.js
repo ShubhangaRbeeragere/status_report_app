f794cd174239ee57c0163357541f7d642c3f8909
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2022-01-12 10:23:36 +0530

    Added formAlignment Class >> for centering the elements inside form

--- status-report-app/src/statusReportPage/components/toDoList/addList.js
+++ status-report-app/src/statusReportPage/components/toDoList/addList.js
@@ -24,46 +24,52 @@ export const AddList = (props) => {
                 >
                     <i className="fas fa-times"></i>
                 </button>
-                <label>
-                    Project:
-                    <br />
+                <div className="formAlignment">
+                    <label>
+                        Project:
+                        <br />
+                        <input
+                            type="text"
+                            value={addList.project}
+                            name="project"
+                            onChange={(e) => {
+                                inputValidate(e);
+                            }}
+                            autoFocus
+                        />
+                    </label>
+                    <label>
+                        Date:
+                        <br />
+                        <input
+                            type="date"
+                            value={addList.date}
+                            name="date"
+                            onChange={(e) => {
+                                inputValidate(e);
+                            }}
+                        />
+                    </label>
+                    <label>
+                        Content:
+                        <br />
+                        <textarea
+                            name="content"
+                            value={addList.content}
+                            id="content"
+                            cols="30"
+                            rows="10"
+                            onChange={(e) => {
+                                inputValidate(e);
+                            }}
+                        ></textarea>
+                    </label>
                     <input
-                        type="text"
-                        value={addList.project}
-                        name="project"
-                        onChange={(e) => {
-                            inputValidate(e);
-                        }}
-                        autoFocus
+                        className="submitButton"
+                        type="submit"
+                        value="Save"
                     />
-                </label>
-                <label>
-                    Date:
-                    <br />
-                    <input
-                        type="date"
-                        value={addList.date}
-                        name="date"
-                        onChange={(e) => {
-                            inputValidate(e);
-                        }}
-                    />
-                </label>
-                <label>
-                    Content:
-                    <br />
-                    <textarea
-                        name="content"
-                        value={addList.content}
-                        id="content"
-                        cols="30"
-                        rows="10"
-                        onChange={(e) => {
-                            inputValidate(e);
-                        }}
-                    ></textarea>
-                </label>
-                <input className="submitButton" type="submit" value="Save" />
+                </div>
             </form>
         </div>
     );
