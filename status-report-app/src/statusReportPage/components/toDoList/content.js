b74e1c095e6c494baa3b22f825ac0864dc9e70ff
Shubhanga R B <shubhanga.rb@travancoreanalytics.com>
2022-01-13 10:53:42 +0530

    Added Message To Content >> when the content of the project is empty a message is required to let the user know that, to add content click on the button.

--- status-report-app/src/statusReportPage/components/toDoList/content.js
+++ status-report-app/src/statusReportPage/components/toDoList/content.js
@@ -1,22 +1,33 @@
 export const Content = (props) => {
-  let deleteContent = props.deleteContent;
-  let content = (
-    <ul>
-      {props.contentArray.map((contentData) => (
-        <li key={contentData.content_id.toString()}>
-          <p>{contentData.content}</p>
-          <button
-            title="delete content"
-            className="removeContentButton"
-            onClick={() => {
-              deleteContent(props.project, contentData.content);
-            }}
-          >
-            <i className="fas fa-minus"></i>
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
-  return <div className="content">{content}</div>;
+    let deleteContent = props.deleteContent;
+    console.log(props.contentArray.length);
+    let content = (
+        <ul>
+            {props.contentArray.map((contentData) => (
+                <li key={contentData.content_id.toString()}>
+                    <p>{contentData.content}</p>
+                    <button
+                        title="delete content"
+                        className="removeContentButton"
+                        onClick={() => {
+                            deleteContent(props.project, contentData.content);
+                        }}
+                    >
+                        <i className="fas fa-minus"></i>
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+    return (
+        <div className="content">
+            {props.contentArray.length === 0 ? (
+                <span className="message">
+                    Press The Below Button To Add Content
+                </span>
+            ) : (
+                content
+            )}
+        </div>
+    );
 };
